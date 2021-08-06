f4d41b7c935e015de6c508d6a7f495a660a15dea
Puneeth <85823685+Xlcr-02@users.noreply.github.com>
2021-08-06 13:19:25 +0530

    India Vs England 50-50 Match added

---
+++ Python/India Vs England 50-50 Match.py
@@ -0,0 +1,16 @@
+#India Vs England 50-50 Match
+
+'''Virat Kohli has won the toss against England in a 50 Over World Cup Final 2019. During the Toss time the commentator have him a funny task to test his mathematical skills. 
+Shastri was the umpire to judge his mathematical skills. When the number is 28 he needs tell "INDIA" and when the number is 25 he needs to tell "ENGLAND".
+
+Refer sample Input and Output:
+
+Input 1: 20 Output: INDIA
+Input 2: 21 Output: ENGLAND
+Input 3: 22 Output: INDIA'''
+
+a=int(input())
+if((a%2)==0):
+ print("INDIA")
+else:
+ print("ENGLAND")
