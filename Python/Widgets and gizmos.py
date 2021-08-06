a9fa10507468176648bf1fcc548c538ed56b8c7b
Puneeth <85823685+Xlcr-02@users.noreply.github.com>
2021-08-06 13:06:42 +0530

    Widgets and gizmos added

---
+++ Python/Widgets and gizmos.py
@@ -0,0 +1,16 @@
+#Widgets and gizmos
+
+'''An online retailer sells two products: widgets and gizmos.
+Each widget weighs 75 grams.
+Each gizmo weighs 112 grams.
+
+Write a program that reads the number of widgets and the number of gizmos in an order from the user.
+Then your program should compute and display the total weight of the order.
+
+Declare Widgest and gizmos as:
+wid=75
+giz=112'''
+
+a=int(input())
+b=int(input())
+print((a*75+b*112)/1000)
