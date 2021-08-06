d3641e80e34592595fab6300e2bae4a6dfe4bd68
Puneeth <85823685+Xlcr-02@users.noreply.github.com>
2021-08-06 12:59:33 +0530

    Convert int to float added

---
+++ Python/Convert integer to float.py
@@ -0,0 +1,10 @@
+#Convert integer to float
+'''Given an input as integer to the machine , he need to get the output of corresponding floating point number
+
+Hint: Divide the number by 100 and display the output
+
+Please help him to writethe program that satisfy the output'''
+
+a=int(input())
+print(a/100)
+
