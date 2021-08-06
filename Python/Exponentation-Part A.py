ae246407295b4bb4b8a09c3132f5e6e701446e3b
Puneeth <85823685+Xlcr-02@users.noreply.github.com>
2021-08-06 14:02:17 +0530

    Exponentation-Part A added

---
+++ Python/Exponentation-Part A.py
@@ -0,0 +1,21 @@
+#Exponentation-Part A
+
+'''
+Write a program to find the exponentation of given number
+
+Input 1: Number of terms 
+Input 2: Base
+
+Output:
+List of exponentation terms for the given input
+
+Refer sample input and output for formatting specification.
+'''
+
+n= int(input())
+b= int(input())
+p = 0
+print("The total terms is:",n)
+for i in range(0, n):
+    p=pow(b,i)
+    print(p)
