dbdbf2d7b910850ccdeb098d60175bc238b88c86
Puneeth <85823685+Xlcr-02@users.noreply.github.com>
2021-08-06 13:52:54 +0530

    pH level added

---
+++ Python/pH Level.py
@@ -0,0 +1,17 @@
+# Ph Level
+
+'''
+Write a Python Program to identify Ph Level Name using the following condition.
+
+Ph <7.0
+acidic Ph>7.0
+basic Ph=0 neutral
+'''
+
+ph=float(input())
+if(ph==0):
+    print("neutral")
+elif(ph<7.0):
+    print("acidic")
+else:
+    print("basic")
